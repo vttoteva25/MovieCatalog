64325273c84e870879d3324a8ddb8ba0c1f0b754
vttoteva25 <stu2201322010@uni-plovdiv.bg>
2024-02-08 16:13:46 +0200

    Add project files.

---
+++ MC.WebApiServices/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using MC.WebApiServices.Models;
+
+namespace MC.WebApiServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello world!";
+        }
+
+        [HttpGet("hello")]
+        public string Get([FromQuery] int count)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i=0; i < count; i++)
+            {
+                stringBuilder.AppendLine("Hello world!");
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        /// <summary>
+        /// Method that return Hello list.
+        /// </summary>
+        /// <param name="name">Name.</param>
+        /// <param name="count">Count.</param>
+        /// <returns>List with specific count of hellos.</returns>
+        [HttpGet("names/{name}")]
+        public string Get([FromRoute] string name, [FromQuery] int count)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < count; i++)
+            {
+                stringBuilder.AppendLine($"Hello {name}!");
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        /// <summary>
+        /// Method that return the Perimeter and the Area of rectangle
+        /// </summary>
+        /// <param name="a">first side.</param>
+        /// <param name="b">second side.</param>
+        /// <returns></returns>
+        [HttpGet("calculator")]
+        [ProducesResponseType(typeof(Rectangle), StatusCodes.Status200OK)]
+        public IActionResult Calculate([FromQuery] string a, [FromQuery] string b)
+        {           
+            return Ok(new Rectangle(a, b));           
+        }
+    }
+}
---
+++ MC.WebApiServices/Models/Rectangle.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MC.WebApiServices.Models
+{
+    public class Rectangle
+    {
+        private double a {  get; set; }
+        private double b { get; set; }
+
+        [JsonPropertyName("Perimeter")]
+        public string Perimeter { get; set; }
+
+        [JsonPropertyName("Area")]
+        public string Area { get; set; }
+
+        public Rectangle(string a, string b)
+        {
+            this.a = double.Parse(a);
+            this.b = double.Parse(b);
+            this.Perimeter = CalculatePerimeter();
+            this.Area = CalculateArea();
+        }
+
+        private string CalculatePerimeter()
+        {
+            return (2 * a + 2 * b).ToString();
+        }
+
+        private string CalculateArea()
+        {
+            return (a * b).ToString();
+        }
+    }
+}
